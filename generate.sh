9d0bbb8eb6a01266071d5a5f97976734aa5efb02
Bram Neijt <bram@neijt.nl>
2022-06-14 22:41:57 +0200

    Add py310

--- generate.sh
+++ generate.sh
@@ -5,11 +5,14 @@ mkdir tags
 
 # The matrix
 cargo run -- --include Pyenv39
+cargo run -- --include Pyenv39 --include A
 cargo run -- --include Pyenv39 --include Stripped
+cargo run -- --include Pyenv310
+cargo run -- --include Pyenv310 --include A
+cargo run -- --include Pyenv310 --include Stripped
 cargo run -- --include Tf12
 cargo run -- --include Aws
 cargo run -- --include Tf12 --include Aws
-cargo run -- --include Pyenv39 --include A
 
 # Build all tags
 for dockerfile in tags/Dockerfile.*; do
